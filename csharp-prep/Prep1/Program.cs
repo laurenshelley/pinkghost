f3b4cf45a3d3e9bd6e5dc2babf63f89f619b5056
laurenshelley <lauren.r.shelley@gmail.com>
2024-11-09 13:36:48 -0700

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        
     }
 }
